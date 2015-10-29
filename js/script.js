cf75e824dfa3d183ebac8eeb1135e1192a921edf
Janwillemnes <Janwillem.nes@gmail.com>
2015-10-29 09:30:27 +0100

    Beoordeling 1
    29-10-2015

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("hello VERKEERD VERBONDEN KUT");
+
+document.querySelector("input[value='project']").onclick = function(){
+    document.getElementById("project").style.display = "block";
+    document.getElementById("stage").style.display = "none";
+}
+
+document.querySelector("input[value='stage']").onclick = function(){
+    document.getElementById("project").style.display = "none";
+    document.getElementById("stage").style.display = "block";
+}
+
+
